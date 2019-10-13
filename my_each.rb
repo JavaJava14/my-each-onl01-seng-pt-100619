c55bd2caf268c7ee5bbeb00e9ac25a02d69a1f3c
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 19:39:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,12 +6,12 @@ def my_each(words)
 end
 if block_given?
     i = 0
- 
+
     while i < array.length
       yield(array[i])
       i = i + 1
     end
- 
+
     array
   else
     puts "Hey! No block was given!"
